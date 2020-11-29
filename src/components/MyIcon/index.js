f6633d421abdd789dad29b30f722353a8c5a1d82
Alec <alec.paglia@gmail.com>
2020-11-28 19:59:02 -0500

    comments

--- src/components/MyIcon/index.js
+++ src/components/MyIcon/index.js
@@ -1,6 +1,7 @@
 import { Box, Typography } from "@material-ui/core";
 import React from "react";
 
+// Custom icon + label
 const MyIcon = ({ icon, label }) => {
   return (
     <Box display="flex" alignItems="center">
